3fe22eb18e12a145852dacb7fe68e1f7a8ced54c
adibbkfn <149227327+adibbkfn@users.noreply.github.com>
2023-11-18 14:03:15 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1700289293,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/elementmetal-sheet0.png",
+		"images/elementmetal2-sheet0.png",
+		"images/aliengreen_round-sheet0.png",
+		"images/elementstone-sheet0.png",
+		"images/elementexplosive-sheet0.png",
+		"images/elementexplosive2-sheet0.png",
+		"images/elementwood-sheet0.png",
+		"images/aliengreen_round2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
